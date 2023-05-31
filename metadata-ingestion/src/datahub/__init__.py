6a68b469389c5389400b7e37f319856aed26e42c
ssogunle-tc <ssogunle@tucowsinc.com>
2023-05-31 20:07:58 +0100

    add version and package name

--- metadata-ingestion/src/datahub/__init__.py
+++ metadata-ingestion/src/datahub/__init__.py
@@ -2,8 +2,9 @@ import sys
 import warnings
 
 # Published at https://pypi.org/project/acryl-datahub/.
-__package_name__ = "acryl-datahub"
-__version__ = "0.0.0.dev0"
+__package_name__ = "acryl-datahub-tc"
+__version__ = "0.10.2.rc1"
+ 
 
 
 def is_dev_mode() -> bool:
