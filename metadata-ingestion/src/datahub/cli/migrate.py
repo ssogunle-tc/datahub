399e0998e01c989471a53c53b3826f4697788b74
ssogunle-tc <ssogunle@tucowsinc.com>
2023-06-05 18:56:19 +0100

    Fix rest_emmiter initializer

--- metadata-ingestion/src/datahub/cli/migrate.py
+++ metadata-ingestion/src/datahub/cli/migrate.py
@@ -140,6 +140,8 @@ def dataplatform2instance_func(
     migration_report = MigrationReport(run_id, dry_run, keep)
     system_metadata = SystemMetadataClass(runId=run_id)
 
+    rest_emitter = None # add default
+    
     if not dry_run:
         rest_emitter = DatahubRestEmitter(
             gms_server=cli_utils.get_session_and_host()[1]
