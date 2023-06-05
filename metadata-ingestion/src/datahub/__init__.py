fa97354179eaf89569f953b2aef426d719f20bd8
ssogunle-tc <ssogunle@tucowsinc.com>
2023-06-05 19:11:42 +0100

    update to 0.10.2.3

--- metadata-ingestion/src/datahub/__init__.py
+++ metadata-ingestion/src/datahub/__init__.py
@@ -3,7 +3,7 @@ import warnings
 
 # Published at https://pypi.org/project/acryl-datahub/.
 __package_name__ = "acryl-datahub-tc"
-__version__ = "0.10.2.0rc4"
+__version__ = "0.10.2.3"
 
 
 def is_dev_mode() -> bool:
